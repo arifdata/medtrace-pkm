7d207dd3b297e8a47828b5680e40314808cc58f4
Arif Mulpratama <arifdata7@gmail.com>
2025-04-29 09:55:17 +0700

    new collections: buku_penerimaan_gudang, stok_gudang

---
+++ pb_migrations/1745894817_created_buku_penerimaan_gudang.js
@@ -0,0 +1,130 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = new Collection({
+    "createRule": null,
+    "deleteRule": null,
+    "fields": [
+      {
+        "autogeneratePattern": "[a-z0-9]{15}",
+        "hidden": false,
+        "id": "text3208210256",
+        "max": 15,
+        "min": 15,
+        "name": "id",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": true,
+        "required": true,
+        "system": true,
+        "type": "text"
+      },
+      {
+        "hidden": false,
+        "id": "date3470571485",
+        "max": "",
+        "min": "",
+        "name": "tanggal_terima",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "date"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "pbc_3590399593",
+        "hidden": false,
+        "id": "relation3512905749",
+        "maxSelect": 1,
+        "minSelect": 0,
+        "name": "nama_bmhp",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "hidden": false,
+        "id": "number2660322358",
+        "max": null,
+        "min": null,
+        "name": "jumlah",
+        "onlyInt": true,
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "number"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text1428707618",
+        "max": 0,
+        "min": 0,
+        "name": "nomor_batch",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": false,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "hidden": false,
+        "id": "number2396831625",
+        "max": null,
+        "min": null,
+        "name": "harga_satuan",
+        "onlyInt": false,
+        "presentable": false,
+        "required": false,
+        "system": false,
+        "type": "number"
+      },
+      {
+        "hidden": false,
+        "id": "date3527179151",
+        "max": "",
+        "min": "",
+        "name": "tanggal_kadaluarsa",
+        "presentable": false,
+        "required": false,
+        "system": false,
+        "type": "date"
+      },
+      {
+        "hidden": false,
+        "id": "autodate2990389176",
+        "name": "created",
+        "onCreate": true,
+        "onUpdate": false,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      },
+      {
+        "hidden": false,
+        "id": "autodate3332085495",
+        "name": "updated",
+        "onCreate": true,
+        "onUpdate": true,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      }
+    ],
+    "id": "pbc_740636071",
+    "indexes": [],
+    "listRule": null,
+    "name": "buku_penerimaan_gudang",
+    "system": false,
+    "type": "base",
+    "updateRule": null,
+    "viewRule": null
+  });
+
+  return app.save(collection);
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_740636071");
+
+  return app.delete(collection);
+})
