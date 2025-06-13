b421ddea919e427ef22fcfa345b5bd1d14a72d8c
Arif Mulpratama <arifdata7@gmail.com>
2025-06-13 11:20:02 +0700

    learning svelte and pico css

--- frontend-js/src/App.svelte
+++ frontend-js/src/App.svelte
@@ -5,73 +5,25 @@
   import '@picocss/pico'
   import Router from 'svelte-spa-router'
   import routes from './routes'
+
+  import Sun from './lib/svg/sun-material.svelte'
+  import Moon from './lib/svg/moon-material.svelte'
 </script>
+
+<main class="container">
 
 <nav>
   <ul>
-    <li><strong>Acme Corp</strong></li>
+    <li><strong>MedTrace PKM</strong></li>
+    <li><a style="text-decoration:none" href="/">Home</a></li>
+    <li><button class="contrast"><Moon /></button></li>
   </ul>
+			
   <ul>
-    <li><a href="#/">Home</a></li>
-    <li><a href="#/hello/svelte">Say hi!</a></li>
-    <li><a href="#/wild/card">Wildcard route</a></li>
-    <li><a href="#/does/not/exist">Not found</a></li>
+    <li><a href="#">About</a></li>
+	<li><a href="#">Products</a></li>
+	<li><a href="#">Products2</a></li>
   </ul>
 </nav>
 
-<!-- Show the router -->
-<Router {routes} />
-
-<main>
-  
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
-
-<fieldset>
-  <label>
-    <input name="2fa" type="checkbox" role="switch" aria-invalid="false" />
-    Pico CSS Test 1
-  </label>
-  <label>
-    <input name="subscription" type="checkbox" role="switch" aria-invalid="true" />
-    Pico CSS Test 2
-  </label>
-</fieldset>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
