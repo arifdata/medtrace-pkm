653e967307fe8092333f5cf5a6c9c917ff37e396
Arif Mulpratama <arifdata7@gmail.com>
2025-06-19 22:27:11 +0700

    install faker package for generate massive datas

--- frontend-js/src/routes/DataPasienPage.svelte
+++ frontend-js/src/routes/DataPasienPage.svelte
@@ -1,29 +1,42 @@
 <script>
-  import client from '../pb/client'
+  import client from "../pb/client";
+  import { fakerID_ID as faker } from "@faker-js/faker";
 
-  async function listPasien(){
-  	const resultList = await client.collection('data_pasien').getList(1, 5, {
-  	  sort: 'nama_pasien', //bisa juga -nama_pasien
-  	});
-  	return resultList;
+  async function generateDataPasien() {
+    // example create data
+    const data = {
+      nama_pasien: faker.person.fullName(),
+      nomor_kartu: faker.finance.accountNumber(),
+      alamat: faker.location.city(),
+      tanggal_lahir: faker.date.birthdate(),
+      nomor_telepon: faker.phone.number(),
+    };
+
+    const record = await client.collection("data_pasien").create(data);
   }
 
+  async function listPasien() {
+    const resultList = await client.collection("data_pasien").getList(1, 50, {
+      sort: "nama_pasien", //bisa juga -nama_pasien
+    });
+    return resultList;
+  }
 
-  import { SkeletonText } from "carbon-components-svelte";
+  import { SkeletonText, Button } from "carbon-components-svelte";
 </script>
+
 <h4>Data Pasien</h4>
+<Button on:click={generateDataPasien}>Generate Data</Button><br />
 
 {#await listPasien()}
   <SkeletonText />
 {:then val}
   {#if val["totalItems"] != 0}
-	  {#each val["items"] as data}
-	  	{data.nama_pasien}<br>
-	  {/each}
+    {#each val["items"] as data}
+      {data.nama_pasien}<br />
+    {/each}
   {:else}
-  <p>
-  Belum ada data pasien
-  </p>
+    <p>Belum ada data pasien</p>
   {/if}
 {:catch error}
   Login untuk akses halaman ini.
