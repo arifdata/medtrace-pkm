b27bf935672f798b14034b65ef0f709fe9dc9a08
Arif Mulpratama <arifdata7@gmail.com>
2025-06-19 13:14:13 +0700

    still working on listing data pasien

--- frontend-js/src/routes/DataPasienPage.svelte
+++ frontend-js/src/routes/DataPasienPage.svelte
@@ -1,26 +1,30 @@
 <script>
   import client from '../pb/client'
 
-  async function cobaListPasien(){
-  	const resultList = await client.collection('data_pasien').getList(1, 2, {
+  async function listPasien(){
+  	const resultList = await client.collection('data_pasien').getList(1, 5, {
   	  sort: 'nama_pasien', //bisa juga -nama_pasien
   	});
-
-  	//const data = await resultList.json();
   	return resultList;
   }
 
-  let a = cobaListPasien();
-  console.log(a);
+
+  import { SkeletonText } from "carbon-components-svelte";
 </script>
 <h4>Data Pasien</h4>
 
-{#await a}
-  ... Loading
+{#await listPasien()}
+  <SkeletonText />
 {:then val}
-  {#each val["items"] as data}
-  	{data.nama_pasien}<br>
-  {/each}
+  {#if val["totalItems"] != 0}
+	  {#each val["items"] as data}
+	  	{data.nama_pasien}<br>
+	  {/each}
+  {:else}
+  <p>
+  Belum ada data pasien
+  </p>
+  {/if}
 {:catch error}
-  {error}
+  Login untuk akses halaman ini.
 {/await}
---
+++ pb_migrations/1750306392_updated_data_pasien.js
@@ -0,0 +1,20 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_45206674")
+
+  // update collection data
+  unmarshal({
+    "listRule": null
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_45206674")
+
+  // update collection data
+  unmarshal({
+    "listRule": ""
+  }, collection)
+
+  return app.save(collection)
+})
