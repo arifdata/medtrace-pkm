d110ae49263a31d46d4ce7b167476ee689772bae
Arif Mulpratama <arifdata7@gmail.com>
2025-06-25 21:31:15 +0700

    install flatpickr supaya bisa translate datepicker ke bhs Indonesia

--- frontend-js/src/routes/pendaftaran/TambahPasienPage.svelte
+++ frontend-js/src/routes/pendaftaran/TambahPasienPage.svelte
@@ -1,41 +1,45 @@
 <script>
-  import { TextInput, Button, DatePicker, DatePickerInput } from "carbon-components-svelte";
-  let namaPasienInput = {value: ""};
+  import {
+    TextInput,
+    Button,
+    DatePicker,
+    DatePickerInput,
+  } from "carbon-components-svelte";
+  import { Indonesian } from "flatpickr/dist/l10n/id.js";
+
+  let namaPasienInput = { value: "" };
   let nomorKartu = "";
-  let alamatInput = {value: ""};
+  let alamatInput = { value: "" };
   let tgl;
-
-
 </script>
 
 <h4>Tambah pasien</h4>
 
-
 <Button
   on:click={() => {
     console.log(tgl);
-  }}
-  >Coba</Button>
+  }}>Coba</Button
+>
 
 <TextInput
   light
   size="sm"
-  warn={namaPasienInput['warn']}
-  warnText={namaPasienInput['warnText']}
+  warn={namaPasienInput["warn"]}
+  warnText={namaPasienInput["warnText"]}
   labelText="Nama"
   placeholder="Masukkan nama pasien..."
   on:input={(val) => {
-    if (val['detail'] == "") {
-      namaPasienInput['warnText'] = "Nama tidak boleh kosong";
-      namaPasienInput['warn'] = true;
-      namaPasienInput['value'] = val['detail'];
-    } else if (val['detail'].length > 100) {
-      namaPasienInput['warnText'] = "Maksimal 100 karakter";
-      namaPasienInput['warn'] = true;
-      namaPasienInput['value'] = val['detail'];
+    if (val["detail"] == "") {
+      namaPasienInput["warnText"] = "Nama tidak boleh kosong";
+      namaPasienInput["warn"] = true;
+      namaPasienInput["value"] = val["detail"];
+    } else if (val["detail"].length > 100) {
+      namaPasienInput["warnText"] = "Maksimal 100 karakter";
+      namaPasienInput["warn"] = true;
+      namaPasienInput["value"] = val["detail"];
     } else {
-      namaPasienInput['warn'] = false;
-      namaPasienInput['value'] = val['detail'];
+      namaPasienInput["warn"] = false;
+      namaPasienInput["value"] = val["detail"];
     }
   }}
 />
@@ -46,29 +50,36 @@
   labelText="Nomor Kartu"
   placeholder="Masukkan nomor kartu..."
   on:input={(val) => {
-    nomorKartu = val['detail'];
+    nomorKartu = val["detail"];
   }}
 />
 
 <TextInput
   light
   size="sm"
-  warn={alamatInput['warn']}
-  warnText={alamatInput['warnText']}
+  warn={alamatInput["warn"]}
+  warnText={alamatInput["warnText"]}
   labelText="Alamat"
   placeholder="Masukkan alamat pasien..."
   on:input={(val) => {
-    if (val['detail'] == "") {
-      alamatInput['warnText'] = "Alamat tidak boleh kosong";
-      alamatInput['warn'] = true;
-      alamatInput['value'] = val['detail'];
+    if (val["detail"] == "") {
+      alamatInput["warnText"] = "Alamat tidak boleh kosong";
+      alamatInput["warn"] = true;
+      alamatInput["value"] = val["detail"];
     } else {
-      alamatInput['warn'] = false;
-      alamatInput['value'] = val['detail'];
+      alamatInput["warn"] = false;
+      alamatInput["value"] = val["detail"];
     }
   }}
 />
 
-<DatePicker light datePickerType="single" on:change bind:value={tgl} dateFormat="d-m-Y">
+<DatePicker
+  locale={Indonesian}
+  light
+  datePickerType="single"
+  on:change
+  bind:value={tgl}
+  dateFormat="d-m-Y"
+>
   <DatePickerInput labelText="Tanggal Lahir" placeholder="mm/dd/yyyy" />
 </DatePicker>
