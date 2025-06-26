7c5b22b81951bf46905ea6f9539bc4bf3e31fade
Arif Mulpratama <arifdata7@gmail.com>
2025-06-26 11:09:13 +0700

    toastify berhasil dan gagal input data

--- frontend-js/src/utils/crudUtils.js
+++ frontend-js/src/utils/crudUtils.js
@@ -1,4 +1,6 @@
 import { client } from '../pb/client';
+  import Toastify from "toastify-js";
+  import "toastify-js/src/toastify.css";
 
 export async function tambahDataPasien(nama, nomor_kartu, alamat, tanggal_lahir, nomor_telepon) {
     // example create data
@@ -14,9 +16,24 @@ export async function tambahDataPasien(nama, nomor_kartu, alamat, tanggal_lahir,
       await client
         .collection("data_pasien")
         .create(data)
-        .then((data) => console.log(data["nama_pasien"]));
+        .then(() => Toastify({
+        text: `Berhasil input ${data["nama_pasien"]}`,
+        duration: 3000,
+        gravity: "bottom",
+        style: {
+          background: "#42be65",
+        },
+      }).showToast()
+);
     } catch (e) {
-      console.log(e);
+      Toastify({
+        text: "Gagal input data.",
+        duration: 3000,
+        gravity: "bottom",
+        style: {
+          background: "#fa4d56",
+        },
+      }).showToast();
     }
   }
 
