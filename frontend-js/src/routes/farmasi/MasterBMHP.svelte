0928573d0f12858d410bdf7e32327bb34ec1c43f
Arif Mulpratama <arifdata7@gmail.com>
2025-07-02 14:32:56 +0700

    modal utk nambah sumber baru

--- frontend-js/src/routes/farmasi/MasterBMHP.svelte
+++ frontend-js/src/routes/farmasi/MasterBMHP.svelte
@@ -1,1 +1,26 @@
+<script>
+  import { Button, Modal, TextInput } from "carbon-components-svelte";
+
+  let modalSumberOpened = false;
+</script>
 <h4>Master BMHP</h4>
+
+<Button size="small" on:click={() => (modalSumberOpened = true)} >Tambah sumber BMHP</Button>
+
+<Modal
+    bind:open={modalSumberOpened}
+    modalHeading="Tambah sumber BMHP baru"
+    primaryButtonText="Submit"
+    secondaryButtonText="Cancel"
+    selectorPrimaryFocus="#form-sumber"
+    on:click:button--secondary={() => (modalSumberOpened = false)}
+    on:open
+    on:close
+    on:submit
+  >
+      <TextInput
+    id="form-sumber"
+    labelText="Sumber BMHP"
+    placeholder="Masukkan sumber BMHP..."
+  />
+    </Modal>
