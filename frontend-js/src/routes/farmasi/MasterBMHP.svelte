cb90b44f9d7a1d29b260c8f2cf44de43d3400a4e
Arif Mulpratama <arifdata7@gmail.com>
2025-07-06 08:41:10 +0700

    add new sumber

--- frontend-js/src/routes/farmasi/MasterBMHP.svelte
+++ frontend-js/src/routes/farmasi/MasterBMHP.svelte
@@ -1,19 +1,16 @@
 <script>
   import { Tabs, Tab, TabContent } from "carbon-components-svelte";
-  import SumberBMHP from './master_bmhp_components/SumberBMHP.svelte'
+  import SumberBMHP from "./master_bmhp_components/SumberBMHP.svelte";
 </script>
 
 <Tabs>
-    <Tab label="Master BMHP" />
-    <Tab label="Sumber BMHP" />
-    <Tab label="Tab label 3" />
-    <svelte:fragment slot="content">
-      <TabContent>Content 1</TabContent>
-      <TabContent>
-        <SumberBMHP />
-      </TabContent>
-      <TabContent>Content 3</TabContent>
-    </svelte:fragment>
+  <Tab label="Master BMHP" />
+  <Tab label="Sumber BMHP" />
+
+  <svelte:fragment slot="content">
+    <TabContent>Content 1</TabContent>
+    <TabContent>
+      <SumberBMHP />
+    </TabContent>
+  </svelte:fragment>
 </Tabs>
-
-
--- frontend-js/src/routes/farmasi/master_bmhp_components/SumberBMHP.svelte
+++ frontend-js/src/routes/farmasi/master_bmhp_components/SumberBMHP.svelte
@@ -1,23 +1,105 @@
 <script>
-  import { Button, Modal, TextInput } from "carbon-components-svelte";
+  import { client } from "../../../pb/client";
+  import {
+    Button,
+    Modal,
+    TextInput,
+    ButtonSet,
+    Grid,
+    Row,
+    Column,
+    UnorderedList,
+    ListItem,
+    SkeletonText,
+  } from "carbon-components-svelte";
+
+  import { Add, Renew } from "carbon-icons-svelte";
+
   let modalSumberOpened = false;
+  let valueNewSumber = "";
+
+  async function listFullSumber() {
+    const resultList = await client.collection("sumber").getFullList();
+    return resultList;
+  }
+
+  async function createNewSumber(value) {
+    const data = {
+      sumber: value,
+    };
+    await client.collection("sumber").create(data);
+  }
+
+  let promiseListSumber = listFullSumber();
+  console.log(promiseListSumber[0]);
 </script>
 
-<Button size="small" on:click={() => (modalSumberOpened = true)} >Tambah sumber BMHP</Button>
 <Modal
-    bind:open={modalSumberOpened}
-    modalHeading="Tambah sumber BMHP baru"
-    primaryButtonText="Submit"
-    secondaryButtonText="Cancel"
-    selectorPrimaryFocus="#form-sumber"
-    on:click:button--secondary={() => (modalSumberOpened = false)}
-    on:open
-    on:close
-    on:submit
-  >
-      <TextInput
+  bind:open={modalSumberOpened}
+  modalHeading="Tambah sumber BMHP baru"
+  primaryButtonText="Submit"
+  secondaryButtonText="Cancel"
+  selectorPrimaryFocus="#form-sumber"
+  on:click:button--secondary={() => {
+    modalSumberOpened = false;
+    valueNewSumber = "";
+  }}
+  on:open
+  on:close
+  on:submit={() => {
+    createNewSumber(valueNewSumber).then(() => {
+      valueNewSumber = "";
+      promiseListSumber = listFullSumber();
+      modalSumberOpened = false;
+    });
+  }}
+>
+  <TextInput
     id="form-sumber"
     labelText="Sumber BMHP"
     placeholder="Masukkan sumber BMHP..."
+    bind:value={valueNewSumber}
   />
-    </Modal>
+</Modal>
+
+<Grid noGutterLeft narrow>
+  <Row>
+    <Column><h6>Sumber BMHP</h6></Column>
+    <Column></Column>
+    <Column></Column>
+    <Column
+      ><ButtonSet>
+        <Button
+          icon={Add}
+          size="small"
+          on:click={() => (modalSumberOpened = true)}>Tambah sumber</Button
+        >
+        <Button
+          icon={Renew}
+          size="small"
+          on:click={() => {
+            promiseListSumber = listFullSumber();
+          }}>Muat ulang</Button
+        >
+      </ButtonSet></Column
+    >
+  </Row>
+</Grid>
+
+<br />
+
+{#await promiseListSumber}
+  <SkeletonText />
+{:then val}
+  {#if val.length > 0}
+    <UnorderedList>
+      {#each val as item}
+        <ListItem>{item.sumber}</ListItem>
+      {/each}
+    </UnorderedList>
+  {:else}
+    Belum ada data
+  {/if}
+{:catch error}
+  {error}
+{/await}
