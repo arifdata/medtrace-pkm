15e64e2f33246e28ecb557c68d89c267fc8e390f
Arif Mulpratama <arifdata7@gmail.com>
2025-07-07 11:11:54 +0700

    masih working on menampilkan data tabel master bmhp

--- frontend-js/src/routes/farmasi/master_bmhp_components/TabelMasterBMHP.svelte
+++ frontend-js/src/routes/farmasi/master_bmhp_components/TabelMasterBMHP.svelte
@@ -13,6 +13,8 @@
         Link,
     } from "carbon-components-svelte";
 
+    import { Add, Renew } from "carbon-icons-svelte";
+
     async function listFullMasterBMHP() {
         const resultList = await client.collection("master_bmhp").getFullList({
             sort: "nama_obat",
@@ -21,8 +23,58 @@
         return resultList;
     }
 
+    function generateRowMasterBMHP(data) {
+      let rowData = [];
+      for (let i = 0; i < data.length; i++) {
+        rowData.push({
+          id: data[i].id,
+          "nama-bmhp": data[i].nama_obat,
+          "sumber": data[i].expand.sumber.sumber,
+        });
+      }
+      return rowData;
+    }
+
     let promiseListMaster = listFullMasterBMHP();
+    let pageSize = 20;
+    let page = 1;
     console.log(promiseListMaster);
 </script>
 
-load tabel disini
+{#await promiseListMaster}
+  loading...
+{:then val}
+  {#if val.length > 0}
+    <DataTable
+      title="Master Item BMHP"
+      sortable
+      size="short"
+      zebra
+      expandable
+      stickyHeader
+      headers={[
+        { key: "nama-bmhp", value: "Nama BMHP" },
+        { key: "sumber", value: "Sumber" },
+      ]}
+      rows={generateRowMasterBMHP(val)}
+      {page}
+      {pageSize}
+    >
+      <Toolbar size="sm">
+        <ToolbarContent>
+          <ToolbarSearch persistent shouldFilterRows />
+          <Button icon={Add} href="#/data_pasien/tambah" target="_blank"
+            >Tambah Data</Button
+          >
+          <Button icon={Renew}
+            on:click={() => {
+              console.log("test");
+            }}>Muat ulang</Button
+          >
+        </ToolbarContent>
+      </Toolbar>
+  </DataTable>
+  {:else}
+  belum ada data
+  {/if}
+{/await}
