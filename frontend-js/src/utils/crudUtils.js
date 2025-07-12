e9c95d1eca3ce20bd28a466f36d0da2789cee91c
Arif Mulpratama <arifdata7@gmail.com>
2025-07-12 21:29:46 +0700

    refactoring toast

--- frontend-js/src/utils/crudUtils.js
+++ frontend-js/src/utils/crudUtils.js
@@ -1,40 +1,27 @@
 import { client } from '../pb/client'
-  import Toastify from "toastify-js"
-  import "toastify-js/src/toastify.css"
+import { successToast, errorToast } from './toast';
 
 export async function tambahDataPasien(nama, nomor_kartu, alamat, tanggal_lahir, nomor_telepon) {
-    // example create data
-    const data = {
-      nama_pasien: nama,
-      nomor_kartu: nomor_kartu,
-      alamat: alamat,
-      tanggal_lahir: tanggal_lahir,
-      nomor_telepon: nomor_telepon,
-    };
+  // example create data
+  const data = {
+    nama_pasien: nama,
+    nomor_kartu: nomor_kartu,
+    alamat: alamat,
+    tanggal_lahir: tanggal_lahir,
+    nomor_telepon: nomor_telepon,
+  };
 
-    try {
-      await client
-        .collection("data_pasien")
-        .create(data)
-        .then(() => Toastify({
-        text: `Berhasil input ${data["nama_pasien"]}`,
-        duration: 3000,
-        gravity: "bottom",
-        style: {
-          background: "#42be65",
-        },
-      }).showToast());
-      return true;
-    } catch (e) {
-      Toastify({
-        text: "Gagal input data.",
-        duration: 3000,
-        gravity: "bottom",
-        style: {
-          background: "#fa4d56",
-        },
-      }).showToast();
+  try {
+    await client
+      .collection("data_pasien")
+      .create(data)
+      .then(() => {
+        successToast(`Berhasil input ${data["nama_pasien"]}`);
+      });
+    return true;
+  } catch (e) {
+    errorToast(`Gagal input data ${data["nama_pasien"]}`)
     return false;
-    }
   }
+}
 
