1944a0506ecee91d2cd3d1b75f2b3506907b7820
Arif Mulpratama <arifdata7@gmail.com>
2025-07-29 13:23:04 +0700

    nyoba2 promise

--- frontend-js/src/routes/DashboardPage.svelte
+++ frontend-js/src/routes/DashboardPage.svelte
@@ -1,5 +1,11 @@
 <script>
   import { data, test } from '../store/sumber-store.ts'
+
+  async function delay(time, val) {
+    return new Promise(resolve => setTimeout(resolve, time, val));
+  }
+
+  let x = delay(3000, "Voila");
 </script>
   
 <h4>Dashboard</h4>
@@ -11,3 +17,9 @@
     data.set({value: await test()});
   }}>Coba</button
 >
+
+{#await x}
+  Loading... (3 detik)
+{:then val}
+  {val}
+{/await}
