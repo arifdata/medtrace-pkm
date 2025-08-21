3a404e748c09da04b5f87b47e41832741583d0a3
Arif Mulpratama <arifdata7@gmail.com>
2025-08-21 11:58:50 +0700

    experimenting with JSON.stringify

--- frontend-js/src/routes/farmasi/DetailMasterBMHP.svelte
+++ frontend-js/src/routes/farmasi/DetailMasterBMHP.svelte
@@ -1,12 +1,39 @@
 <script>
+  import { client } from "../../pb/client.js"
   export let params = {};
 
   import { storeSumber, setValueSumber } from "../../store/sumber-store";
 
+  let queriedData = {};
+
+  async function getData(id) {
+    const record = await client.collection('master_bmhp').getOne(id);
+    return record;
+  }
+
+  let coba = getData(params["id"]).then((data) => {
+    queriedData["nama_obat"] = data["nama_obat"];
+    queriedData["sumber"] = data["sumber"];
+    queriedData["is_antibiotik"] = data["is_antibiotik"];
+    queriedData["is_narko_psiko"] = data["is_narko_psiko"];
+  });
+
+
   // setValueSumber();
+  // console.log(yy);
+  // JSON.stringify(yy);
 </script>
 
 <h4>Detail Master BMHP</h4>
-{params["id"]}
+{JSON.stringify(queriedData)}
+<button on:click={() => {
+  console.log(JSON.stringify(queriedData));
+}}>tekan</button>
 
-{$storeSumber}
+<!-- {$storeSumber[0].sumber} -->
+{#await coba}
+
+{:then data}
+
+  test
+{/await}
